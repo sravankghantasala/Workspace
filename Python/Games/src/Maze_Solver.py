bc94f9b7ad8d736e964a0db3a7e93b0bd0eabef3
sravankghantasala <sravan.campus@gmail.com>
2014-10-17 09:57:03 +0530

    Fixed Maze_solver for lateral row movement

--- Python/Games/src/Maze_Solver.py
+++ Python/Games/src/Maze_Solver.py
@@ -5,6 +5,8 @@ Created on 29 Sep 2014
 '''
 from random import shuffle
 import os
+import sys
+import time
 
 def get_size():
     size=int(input('Enter the size of the maze you want to play ... '))
@@ -23,19 +25,44 @@ def game_over(current,size):
 
 def get_move(size):
     while(True):
-        move=int(input('Please enter your move ... '))
+        move=int(input('\nPlease enter your move ... '))
         if move in range(1,size*size): break
     return move
 
 
 def move_me(move,data,size):
+    moved = False
+    
     zero_index=data.index('_')
     num_index=data.index(move)
-    delta=abs(num_index - zero_index)
-    if (delta in [1,size]):
-        data[zero_index] = move
-        data[num_index] = '_'
-        
+    
+#     If number is at the left extreme of the row, the last number from the previous row should not be moved.
+#     Identify if the index is left extreme.
+    if (zero_index % size == 0):
+        delta=(num_index - zero_index)
+        if (delta in [1,size,-size]):
+            data[zero_index] = move
+            data[num_index] = '_'
+            moved = True
+    
+#     If number is at the right extreme of the row, the first number from the next row should not be moved.
+#     Identify if the index is right extreme. 
+    elif (zero_index % size == (size-1)):
+        delta=(num_index - zero_index)
+        if (delta in [-1,size,-size]):
+            data[zero_index] = move
+            data[num_index] = '_'
+            moved = True
+
+#     If number is not at any extremes.
+    else:
+        delta=abs(num_index - zero_index)
+        if (delta in [1,size]):
+            data[zero_index] = move
+            data[num_index] = '_'
+            moved = True
+    
+    return moved
 
 
 def maze_looper(size,orig):
@@ -43,10 +70,9 @@ def maze_looper(size,orig):
     moves=0
     while (game_over(data,size)):
         maze_printer(size, data, moves)
-        move_me(get_move(size),data,size)
-        moves+=1
-        
-        
+        if move_me(get_move(size),data,size): moves+=1
+        else: print('Invalid move!'); time.sleep(1)
+            
 def print_banner(moves):
     print('*'*80)
     print('*','Simple Maze'.center(76),'*')
@@ -58,8 +84,8 @@ def print_banner(moves):
 
 def maze_printer(size,data,moves):
 #     Clearing screen
-    os.system('cls')
-    os.system('clear')
+    if 'win' in sys.platform : os.system('cls')
+    elif 'linux' in sys.platform : os.system('clear')
     print_banner(moves)
     count=0
     for i in data:
